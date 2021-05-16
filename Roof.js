c8c4ec00fe7c1da99b1210d90a78a2e798430e62
BhavishyaKhurana17 <81514923+BhavishyaKhurana17@users.noreply.github.com>
2021-05-16 09:50:45 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,25 @@
+class Roof {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true,
+            'restitution': 0,
+            'friction': 0,
+            'density': 1
+        }
+        this.body = Bodies.rectangle(x,y,width,height, options)
+        this.width = 2000;
+        this.height = 15;
+
+        World.add(world, this.body);
+    }
+    display() {
+        rectMode(CENTER);
+
+        fill("yellow");
+
+    
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
+    }
+
+
+};
